93bbede30e02f7cdbcd0d0dd6f0d63ea0bfbfde0
scott123454@hotmail.co.uk <scott@Scotts-MacBook-Pro.local>
2014-04-02 14:12:51 +0100

    list of student puts

--- directory.rb
+++ directory.rb
@@ -1,27 +1,47 @@
-student_count=9
 #first, we print the list of students
+students = [
+"Steve Musgrave",
+"Sroop Sunar",
+"Colin Marshall",
+"Josh Fail-Brown",
+"Louise Lai",
+"Robin Doble",
+"Alex Wong",
+"Scott Dimmock",
+"Muhanad Al-Rubaiee",
+"Shelley Hope",
+"Will Hall",
+"Oliver Delevingne",
+"Nico",
+"Apostolis",
+"Stefania",
+"Robert Leon",
+"Emma Williams",
+"Joey Wolf",
+"Julie Walker"
+]
 puts "The students of my cohort at Makers Academy"
-puts "------------"
-puts "Steve Musgrave"
-puts "Sroop Sunar"
-puts "Colin Marshall"
-puts "Josh Fail-Brown"
-puts "Louise Lai"
-puts "Robin Doble"
-puts "Alex Wong"
-puts "Scott Dimmock"
-puts "Muhanad Al-Rubaiee"
-puts "Shelley Hope"
-puts "Will Hall"
-puts "Oliver Delevingne"
-puts "Nico"
-puts "Apostolis"
-puts "Stefania"
-puts "Robert Leon"
-puts "Emma Williams"
-puts "Joey Wolf"
-puts "Julie Walker"
+puts "-----------"
+puts students [0]
+puts students [1]
+puts students [2]
+puts students [3]
+puts students [4]
+puts students [5]
+puts students [6]
+puts students [7]
+puts students [8]
+puts students [9]
+puts students [10]
+puts students [11]
+puts students [12]
+puts students [13]
+puts students [14]
+puts students [15]
+puts students [16]
+puts students [17]
+puts students [18]
+puts students [19]
 # finally, we print the total
-puts "Overall, we have #{student_count} great students"
-# it's important that print() doesn't add new line characters
+puts "Overall, we have #{students.length} great students"
 
