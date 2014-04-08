74cebb0872a88a8f5c7b3e938fa9f782c52e7e9d
scott123454@hotmail.co.uk <scott@Scotts-MacBook-Pro.local>
2014-04-08 13:54:01 +0100

    completed student directory

--- directory.rb
+++ directory.rb
@@ -1,13 +1,38 @@
+# def input_csv
+# 	puts "Here is a list of the names and details of the March cohort"
+# 	file = File.new("students101csv.csv") 
+# 	puts file
+# end
+
+
 def input_students
-	puts "Please enter the names of students"
+	puts "Please enter the name of student"
 	puts "To finish, just hit return twice"
 	students =[]
 	name = gets.chomp
+	puts "Please enter the cohort"
+	cohort = gets.chomp
+	cohort_array = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 	while !name.empty? do
-		students << {:name => name, :cohort => :March, :gender => :male}
-			puts "Now we have #{students.length} students"
-			name = gets.chomp
+		cohort_exists = cohort_array.select {|month| month==cohort}
+		if cohort.empty? || cohort_exists.empty?
+			cohort = "March"
 		end
+		
+		cohort = cohort.to_sym
+		students << {:name => name, :cohort => cohort, :gender => :male}
+		if students.length > 1
+			puts "Now we have #{students.length} students"
+		else 
+			puts "Now we have #{students.length} student"
+		end
+		puts "Please enter the name of the student"
+		name = gets.chomp
+		puts "Please enter the cohort"
+		cohort = gets.chomp
+
+	end
 	students
 end
 
@@ -19,13 +44,13 @@ end
 
 def print(listofstudents)
 	listofstudents.each_with_index do |student, index|
-		puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort who is a #{student[:gender]}"
+		puts "#{index + 1}. #{student[:name]} #{student[:cohort]} cohort who is a #{student[:gender]}"
 	end
 end
 
-def select_students(students)
-	students.select{|student| student[:name].start_with? ("A")}
-end
+# def select_students(students)
+# 	students.select{|student| student[:name].start_with? ("A")}
+# end
 
 #def twelve_characters(namelength)
 #	namelength.select{|student| student[:name].length <12}
@@ -45,8 +70,13 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students"
 end	
 
+# name = "Scott"
+# puts name.center(20)
 
-students = input_students
+
+
+
+students = input_students()
 print_header
 print(students)
 print_footer(students)
